809cf7c9f46d27a7d9d001ec054c8be1a9518d60
pitnarii <pitnaree_@outlook.com>
2023-08-06 02:18:11 +0100

    Update readme file

--- contracts/EtherWallet.sol
+++ contracts/EtherWallet.sol
@@ -3,18 +3,25 @@ pragma solidity ^0.8.18;
 
 contract EtherWallet {
 
-    address public _owner;
-    uint public _balance;
+    address public owner;
+    // uint private balance;
     constructor() {
-        _owner = payable(msg.sender);
+        owner = payable(msg.sender);
     }
     receive() external payable {}
     // executed on a call to the contract with empty calldata
     // This function cannot have an argument
     
     function Withdraw(uint _amount) external {
-        require(msg.sender == _owner, "Only owner can call this function");
-        require(_amount <= _balance, "Withdraw amount exceeds limit");
-        _balance -= _amount;
+        require(msg.sender == owner, "Only owner can call this function");
+        // require(_amount <= balance, "Withdraw amount exceeds limit");
+        // _balance -= _amount;
+        // modifier(payable)
+        payable(msg.sender).transfer(_amount);
+        
+    }
+
+    function GetBalance() external view returns(uint) {
+        return address(this).balance;
     }
 }
